ea15e71fe15244894ba2227cc6f3df86398b6cfa
Kyle Kastner <kastnerkyle@gmail.com>
2017-03-14 15:00:19 -0400

    Added WIP code

--- alignment/run_alignment.sh
+++ alignment/run_alignment.sh
@@ -1,25 +1,46 @@
-mkdir -p txt
-for i in wav/*; do
-    bname=$(basename $i .wav)
-    cp raw_txt/"$bname".txt tmp.txt
-    cat tmp.txt | tr "\n" "-" | tr -d "-" | tr -s " " | tr "\." "\. " | tr -s " " > tmp2.txt
-    cat tmp2.txt | tr -d "\(" | tr -d "\)" | sed -e 's/Applause//g' | sed -e 's/&nbs;p;//g' | tr -s " " | tr -s "." > tmp3.txt
-    cat tmp3.txt | sed -e 's/\ \.\ /\./g' | sed -e 's/\ \.//g' | sed -e 's/&n;bsp;//g' | sed -e 's/&nbsp;//g' > tmp4.txt
-    cat tmp4.txt | sed -e 's/\(\.\)\(.\)/\. \2/g' | sed -e 's/THEPRESIDENT//g' | tr -s " " > tmp5.txt
-    cat tmp5.txt | sed -e 's/9;//g' | sed -e 's// /g' | sed -e 's/THE PRESIDENT//g' | tr -d ":" | tr -d "'" | tr -s " " > tmp6.txt
-    cat tmp6.txt | sed -e 's/&nb;sp;//g' | sed -e 's/Laughter//g' | LANG=C sed 's/[\d128-\d255]//g' | sed -e 's/Q //g' | sed -e 's/Q\.//g' | sed -e 's/\<p\>//g' | tr -d "<" | tr -d ">" | tr -s " " > tmp7.txt
-    cat tmp7.txt | sed -e 's/&#39;//g' | tr -s " " > tmp8.txt
-    mv tmp8.txt txt/"$bname".txt
-    rm tmp*txt
-done
+# Need to make this consistent
+txtdir=txt
+if [ ! -d "$txtdir" ]; then
+    mkdir -p txt
+    for i in wav/*; do
+        bname=$(basename $i .wav)
+        cp raw_txt/"$bname".txt tmp.txt
+        cat tmp.txt | tr ";" " " | tr "\n" "-" | tr -d "-" | tr -s " " | tr "\." "\. " | tr -s " " > tmp2.txt
+        cat tmp2.txt | tr -d "\(" | tr -d "\)" | sed -e 's/Applause//g' | sed -e 's/&nbsp//g' | tr -s " " | tr -s "." > tmp3.txt
+        cat tmp3.txt | sed -e 's/\ \.\ /\./g' | sed -e 's/\ \.//g' > tmp4.txt
+        cat tmp4.txt | sed -e 's/\(\.\)\(.\)/\. \2/g' | sed -e 's/THEPRESIDENT//g' | tr -s " " > tmp5.txt
+        cat tmp5.txt | sed -e 's/9//g' | sed -e 's// /g' | sed -e 's/THE PRESIDENT//g' | tr -d ":" | tr -d "'" | tr -s " " > tmp6.txt
+        cat tmp6.txt | sed -e 's/Laughter//g' | LANG=C sed 's/[\d128-\d255]//g' | sed -e 's/Q //g' | sed -e 's/Q\.//g' | sed -e 's/\<p\>//g' | tr -d "<" | tr -d ">" | tr -s " " > tmp7.txt
+        cat tmp7.txt | sed -e 's/&#39//g' | sed -e 's/&mdash/ /g' | sed -e 's/$//g' | sed -e 's/,/ , /g' | sed -e 's/,//g' > tmp8.txt
+        cat tmp8.txt | sed -e 's/\./\. /g' | sed -e 's|/||g' | sed -e 's/\$//g' | sed -e 's/&nbs/ /g' | sed -e 's/ \. /\. /g' | tr -s " " > tmp9.txt
+        cat tmp9.txt | sed -e 's/,//g' | sed -e 's/ \./\./g' | sed -e 's/&n bsp/ /g' | sed -e 's/"//g' | sed -e 's/-//g' | sed -e 's/\[//g' | sed -e 's/\]//g' | tr -s " " > tmp10.txt
+        cat tmp10.txt | sed -e 's/&m dash/ /g' | sed -e 's/\?/\./g' | tr -s "." | tr -s " " > tmp11.txt
+        mv tmp11.txt txt/"$bname".txt
+        rm tmp*txt
+    done
 
-grep -o -E '\w+' txt/* | sort -u -f | cut -d ":" -f 2 | sort -u -f > wordlist.txt
+    grep -o -E '\w+' txt/* | sort -u -f | cut -d ":" -f 2 | sort -u -f > wordlist.txt
 
-for i in txt/*; do
-    python wordsplit.py $i
-done
+    for i in txt/*; do
+        python wordsplit.py $i
+    done
+fi
 
 gentledir=gentle
 if [ ! -d "$gentledir" ]; then
     bash build_gentle.sh
 fi
+
+segdir=seg
+if [ ! -d "$segdir" ]; then
+    mkdir -p seg
+    for i in txt/*; do
+        bname=$(basename $i .txt)
+        wavmatch=wav/"$bname".wav
+        jsonmatch=seg/"$bname".json
+        python gentle/align.py $wavmatch $i > res.json
+        mv res.json $jsonmatch 
+    done
+fi
+
+python wavseg.py spe_1960_0715_kennedy.json
---
+++ alignment/wavseg.py
@@ -0,0 +1,163 @@
+import json
+import sys
+import numpy as np
+
+jsonpath = sys.argv[1]
+print(jsonpath)
+
+with open(jsonpath, 'r') as f:
+    d = json.load(f)
+
+full_transcript = d["transcript"]
+all_words = d["words"]
+"""
+w = all_words[0]
+w.keys()
+[u'case',
+ u'end',
+ u'endOffset',
+ u'phones',
+ u'alignedWord',
+ u'start',
+ u'startOffset',
+ u'word']
+"""
+valid_stop_bounds = []
+valid_start_bounds = []
+stopped = True
+last_valid = -1
+for n, wi in enumerate(all_words):
+    if not stopped and wi['case'] == 'not-found-in-audio':
+        valid_stop_bounds.append(all_words[last_valid]['end'])
+        stopped = True
+    elif stopped and wi['case'] == 'success':
+        stopped = False
+        valid_start_bounds.append(wi['start'])
+    if wi['case'] == 'success':
+        last_valid = n
+assert len(valid_start_bounds) == len(valid_stop_bounds)
+
+bounds = list(zip(valid_start_bounds, valid_stop_bounds))
+base_time_diff = [stop - start for start, stop in bounds]
+split_chunk = 20
+drop_chunk = 4
+
+# remove under X second chunks
+to_keep = [n for n, _ in enumerate(base_time_diff) if _ > drop_chunk]
+bounds = [b for n, b in enumerate(bounds) if n in to_keep]
+time_diff = [stop - start for start, stop in bounds]
+
+def dynamic_match(bounds, words):
+    # gnarly attempt at dynamic programming
+    # to match bounds and words, accounting for "not-found-in-audio" chunks
+    kept = []
+    for nb, b in enumerate(bounds):
+        for nw, w in enumerate(words):
+            if w['case'] == 'not-found-in-audio':
+                # try our best to make contiguous chunks
+                # last_val_w = None
+                # next_val_w = None
+                for y in list(range(nw))[::-1]:
+                    if all_words[y]['case'] != 'not-found-in-audio':
+                        last_val_w = words[y]
+                        break
+
+                for z in list(range(nw + 1, len(all_words))):
+                    if all_words[z]['case'] != 'not-found-in-audio':
+                        next_val_w = all_words[z]
+                        break
+
+                if last_val_w['start'] > b[1]:
+                    break
+                elif next_val_w['end'] < b[0]:
+                    continue
+                elif next_val_w['start'] < b[1]:
+                    if last_val_w['end'] > b[0]:
+                        # only use it if nearby context
+                        if abs(y - nw) < 3:
+                            if abs(z - nw) < 3:
+                                kept.append((nb, w))
+            else:
+                if w['start'] > b[1]:
+                    break
+                elif w['end'] < b[0]:
+                    continue
+                elif w['start'] < b[1]:
+                    if w['end'] < b[1]:
+                        kept.append((nb, w))
+    return kept
+
+
+def split_on_max_gap(words):
+    # split in half on maximum gap
+    starts = []
+    ends = []
+    for nw, w in enumerate(words):
+        if w['case'] != 'not-found-in-audio':
+            starts.append(w['start'])
+            ends.append(w['end'])
+        else:
+            last_val_w = None
+            for y in list(range(nw))[::-1]:
+                if words[y]['case'] != 'not-found-in-audio':
+                    last_val_w = words[y]
+                    break
+
+            next_val_w = None
+            for z in list(range(nw + 1, len(words))):
+                if words[z]['case'] != 'not-found-in-audio':
+                    next_val_w = all_words[z]
+                    break
+
+            if last_val_w is None:
+                if next_val_w is None:
+                    raise ValueError("didn't find any forward or previous valid data")
+                last_val_w = w
+                last_val_w['end'] = next_val_w['start']
+            elif next_val_w is None:
+                if last_val_w is None:
+                    raise ValueError("didn't find any forward or previous valid data")
+                next_val_w = w
+                next_val_w['start'] = last_val_w['end']
+            starts.append(last_val_w['end'])
+            ends.append(next_val_w['start'])
+    # global def for split_chunk
+    if ends[-1] - starts[0] < split_chunk:
+        return words
+    gaps = [s - e for s, e in zip(starts[1:], ends[:-1])]
+    gap_inds = [n + 1 for n in range(len(gaps))]
+    max_ind = np.argmax(gaps)
+    slice_point = gap_inds[max_ind]
+    return (words[:slice_point], words[slice_point:])
+
+
+def recursive_split(words, split_limit):
+    if len(words) > split_limit:
+        r = split_on_max_gap(words)
+        rout = []
+        for ri in r:
+            out = recursive_split(ri, split_limit)
+            rout.append(out)
+        return rout
+    else:
+        return words
+
+kept = dynamic_match(bounds, all_words)
+
+# split > X sec chunks into approximate sentence level...
+to_split = [n for n, _ in enumerate(time_diff) if _ > split_chunk]
+# bound indexes and values
+all_bounds = [(n, b) for n, b in enumerate(bounds)]
+split_bounds = [(n, b) for n, b in enumerate(bounds) if n in to_split]
+
+# find new bounds for split_bounds
+split_bound_indices = [i[0] for i in split_bounds]
+final_bounds = []
+for nb, b in enumerate(bounds):
+    if nb not in split_bound_indices:
+        final_bounds.append(b)
+        continue
+    w_in_b = [k[1] for k in kept if k[0] == nb]
+    r = recursive_split(w_in_b, 10)
+from IPython import embed; embed()
+raise ValueError()

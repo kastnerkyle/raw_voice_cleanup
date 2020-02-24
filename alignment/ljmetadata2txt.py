d3f944eeed15700c353b1dcdb908674a5a02599e
Kyle Kastner <kastnerkyle@gmail.com>
2020-02-24 17:44:22 +0000

    made command for dataset file deletion

--- alignment/ljmetadata2txt.py
+++ alignment/ljmetadata2txt.py
@@ -9,6 +9,7 @@ if len(sys.argv) < 3:
 metadata = sys.argv[1]
 txt_path = sys.argv[2]
 
+skipped_wav = []
 with io.open(metadata, encoding='utf-8') as f:
     for line in f:
         parts = line.strip().split('|')
@@ -19,7 +20,17 @@ with io.open(metadata, encoding='utf-8') as f:
         try:
             content = content.decode("ascii")
         except UnicodeEncodeError:
+            print("Non-ascii input found in {}, skipping - consider deleting the corresponding wav file".format(basename))
+            skipped_wav.append(basename + ".wav")
             continue
         full = os.path.join(txt_path, basename + ".txt")
         with open(full, "w") as of:
             of.writelines([content])
+
+delname = "wav_to_delete_meta.txt"
+with open(delname, "w") as o:
+    o.writelines([s + "\n" for s in skipped_wav])
+
+print("Wrote out proposed deletion filenames to {}".format(delname))
+print("You can use this file by `cd` into the firectory of wav files, and using xargs like:")
+print("xargs -a /home/kkastner/src/raw_voice_cleanup/alignment/wav_to_delete_meta.txt -d'\\n' rm")

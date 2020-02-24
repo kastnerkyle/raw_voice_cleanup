832b944930050b530ab36af88049957539909cc5
Kyle Kastner <kastnerkyle@gmail.com>
2020-02-24 14:59:42 +0000

    changed build to use ATLAS, working on Ubuntu 18.04

--- alignment/install_kaldi.sh
+++ alignment/install_kaldi.sh
@@ -5,11 +5,15 @@
 # copied in from elsewhere...
 cp ../../kaldi_tools_Makefile.mod kaldi/tools/Makefile
 cd kaldi/tools
-#make atlas openfst OPENFST_VERSION=1.4.1
+make atlas openfst OPENFST_VERSION=1.4.1
 #make openblas openfst OPENFST_VERSION=1.6.0
-make openblas openfst OPENFST_VERSION=1.4.1
+#make openblas openfst OPENFST_VERSION=1.4.1
 #--openblas-root=../tools/OpenBLAS/install
 cd ../src
-OPENFST_VER=1.4.1 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+#OPENFST_VER=1.4.1 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no #--openblas-root=../tools/OpenBLAS/install
+OPENFST_VER=1.4.1 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no #--openblas-root=../tools/OpenBLAS/install
+# /usr/lib/x86_64-linux-gnu/
+# manually hack in and find/replace lib path! this is.... sketchy
+sed -i -e "s|\[somewhere\]|/usr/lib/x86_64-linux-gnu/|g" kaldi.mk
 make depend
 cd ../../
